efa876e6ce755509253128870aef8ec6c2a68e08
AlmohammedBilal <iambilasith@gmail.com>
2024-09-04 23:09:06 +0530

    shell py code

---
+++ hello.py
@@ -0,0 +1,20 @@
+###################
+
+#Author :AL]lmohammedBilal
+#Date : 04-09-2002
+
+
+################
+
+
+
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
+
+number = 5
+result = factorial(number)
+print(f"The factorial of {number} is {result}.")
+
